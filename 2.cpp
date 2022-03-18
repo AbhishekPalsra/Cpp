3889936ff7f2b91d6e84c91bbdadd9ca083cf4bd
AbhishekPalsra <abhipalsra@gmail.com>
2022-03-18 16:03:34 +0530

    changed git ignore

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include<iostream> // library contatin build in function and objects 
+
+int main(void)
+{
+    std::cout<<"Namaste World"; // using std is good as all fun and obj are not under std namespace
+
+
+
+
+
+
+
+
+    return 0;
+}
